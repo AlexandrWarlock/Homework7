f5ea972e7df8b91462edc3a6b6083e8c6d0cec86
AlexandrWarlock <alexandrpetrov291094@gmail.com>
2023-05-22 22:48:48 +0300

    Домашнее задание 7

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,7 @@ public class Main {
         int all = 2459000;
         int i = 0;
         int moneySave = 0;
-        while (cost < all) {
-            all = all - cost;
+        while (moneySave < all) {
             i = i +1;
             moneySave = i * cost;
             System.out.println("Месяц " + i + " сумма накоплений равна " + moneySave + " рублей");
@@ -25,23 +24,23 @@ public class Main {
     public static void task2 () {
         System.out.println("Задача 2");
         int i = 0;
-        int j = 10;
         while (i < 10) {
             i++;
             System.out.print(" " + i);
         }
         System.out.println(" ");
-        for (; j > 0; j--) {
+        for (int j = 10; j > 0; j--) {
             System.out.print(" " + j);
         }
+        System.out.println();
     }
     public static void task3 () {
         System.out.println("Задача 3");
         int Y = 12_000_000;
-        int bornPerYear = 17 * 1000000 / 1000;
-        int diedPerYear = 8 * 1000000 / 1000;
+        int born = 17;
+        int died = 8;
         for (int year = 1; year < 10; year++) {
-            Y = Y + bornPerYear - diedPerYear;
+            Y += Y * (born - died) / 1000;
             System.out.println("Год " + year + " численность населения состовляет " + Y + " человек");
         }
     }
@@ -79,17 +78,14 @@ public class Main {
     }
     public static void task7 () {
         System.out.println("Задача 7");
-        int month = 0;
-        for (int fr = 0; month < 31; month++) {
-            fr = fr +1;
-            if ((fr - 2) % 7 == 0 && 0 < fr && fr < 31)
-                System.out.println("Сегодня пятница " + fr + " необходимо подготовить отчет" );
+        int fr = 1;
+        for (; fr < 31; fr += 7) {
+            System.out.println("Сегодня пятница " + fr + " необходимо подготовить отчет");
         }
     }
     public static void task8 () {
         System.out.println("Задача 8");
-        int i = 0;
-        for (; i < 2123; i = i + 79) {
+        for (int i = 0; i < 2123; i += 79) {
             if (i > 1823) {
                 System.out.println(i);
             }
